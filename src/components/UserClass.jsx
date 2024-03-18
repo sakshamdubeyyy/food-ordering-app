bdca1025917b848fa8723907ebdd6b4e9405316a
sakshamdubeyyy <109180511+sakshamdubeyyy@users.noreply.github.com>
2024-03-18 12:02:57 +0530

    Here by explaining the class based component's lifecycle in the About page

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 
 class UserClass extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+        console.log(this.props.name + "Child constructor called")
+    }
+
+    componentDidMount(){
+        console.log(this.props.name + "Child component did mount")
+    }
+
+    componentDidUpdate(){
+        console.log(this.props.name + "component updated")
+    }
+
+
     render(){
-        return(
+        console.log(this.props.name + "Child render called")
+        return( 
             <div className="user">
-                <h1>Name: Saksham</h1>
+                <hr/>
+                <h3>This is a class component</h3>
+                <h2>Count: {this.state.count}</h2>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count + 1,
+                    })
+                }}>Increment </button>
+                <h1>Name: {this.props.name}</h1>
                 <h3>Location: Sultanpur</h3>
                 <h3>Email: sakshamdubeyy23@gmail.com</h3>
+                <hr/>
             </div>
         )
     }
